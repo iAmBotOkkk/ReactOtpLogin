6296c284fe746a46ccfb00a929044b125053b452
iAmBotOkkk <barmanbishal1234@gmail.com>
2025-07-07 16:52:29 +0530

    commit : Updated component

--- src/Components/otpLogin.jsx
+++ src/Components/otpLogin.jsx
@@ -3,8 +3,12 @@
  export const OTPLogin = () =>{
 
     return(
-        <div>
-            
+        <div className="Otp">
+            <h1>Login via OTP</h1>
+            <div className="btn">
+                <input type="number" placeholder="Enter Your mobile number" />
+                <button>Send Otp</button>
+            </div>
         </div>
     )
 }
