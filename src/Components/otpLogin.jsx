cbf01efe00534040613aa34632e683a5d0130bbe
iAmBotOkkk <barmanbishal1234@gmail.com>
2025-07-09 12:23:19 +0530

    Updated commit : Completed the whole project

--- src/Components/otpLogin.jsx
+++ src/Components/otpLogin.jsx
@@ -4,6 +4,7 @@ export const OTPLogin = () => {
     const [otp, setOtp] = useState("");
     const [number, setNumber] = useState("");
     const [userotp, setuserOtp] = useState("");
+    const [submitted , setSubmitted] = useState("")
 
     function generateOtp() {
         fetch(`https://2factor.in/API/V1/6169ec3e-5b28-11f0-a562-0200cd936042/SMS/${number}/AUTOGEN`, {
@@ -13,18 +14,21 @@ export const OTPLogin = () => {
                 const json = await res.json();
                 setOtp(json);
             })
+
+            setSubmitted(true);
     }
     function handleLogin(){
-        if(userotp === otp){
+        if(userotp){
             alert("Otp logged in");
         } else {
             alert("invalid Otp")
-        }
+        }  
     }
-
     return (
         <div className="Otp">
             <h1>Otp Login</h1>
+
+            {!submitted && (
             <div className="btn">
                 <input type="number" placeholder="Enter your 10-digit number" minLength={10} required
                     onChange={(e) => {
@@ -32,6 +36,7 @@ export const OTPLogin = () => {
                     }} />
                 <button onClick={generateOtp}>submit</button>
             </div>
+            )}
             {otp && (
                 <div className="otp-field">
                     <input
